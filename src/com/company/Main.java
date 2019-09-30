dcc15b469fd40092d00cbdaa7c9ec717a918acde
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-09-30 09:55:33 -0400

    declared all my variables

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+     //declare faze
+         Scanner Keyboard;
+    int testing;
+    String question;
+
+    //initialization faze
+   
+
+
+
+
+
+
+
+    }
+}
