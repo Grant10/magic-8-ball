4a946d4aa6aa445bf62279b3e5b565a0fe62eadc
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-10-03 09:38:44 -0400

    initialized one good and two bad

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,12 +26,21 @@ public class Main {
      if(40>=firstNumber&&firstNumber >31){
         System.out.println("That will happen in the near future");
      }
-
-
-
-
-
-
+    if(50>= firstNumber&&firstNumber >41){
+        System.out.println("That is very unlikely");
+    }
+    if(60>=firstNumber&&firstNumber >51){
+        System.out.println("no");
+    }
+    if(70>=firstNumber&&firstNumber >61){
+        System.out.println("probably");
+    }
+    if(80>=firstNumber&&firstNumber >71){
+        System.out.println("probably not");
+    }
+    if(90>=firstNumber&&firstNumber >81){
+        System.out.println("")
+    }
 
     }
 }
