dd818b86d57057dbc54caa132f321823a4ecefdb
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-10-03 09:26:14 -0400

    initialized two more goods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,11 +15,17 @@ public class Main {
         System.out.println("type a number 1-100 and give me a question");
     int firstNumber = Keyboard.nextInt();
     if(10>=firstNumber&&firstNumber >0) {
-        System.out.println("the outlook is very good");
+        System.out.println("the outlook looks very good");
     }
             if (20>=firstNumber&&firstNumber >11){
             System.out.println("The outlook doesn't look very good");
         }
+     if(30>=firstNumber&&firstNumber >21) {
+        System.out.println("yes");
+     }
+     if(40>=firstNumber&&firstNumber >31){
+        System.out.println("That will happen in the near future");
+     }
 
 
 
