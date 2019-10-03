8a8e760f76004bb09c1e6bb2fd6206ff4fb0b0f2
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-10-03 09:48:27 -0400

    added code to put that is not in the number range if it isn't in range

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,42 +5,47 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-     //declare faze
+        //declare faze
         Scanner Keyboard;
-    int testing;
-    String question;
+        int testing;
+        String question;
 
-    //initialization faze
-    Keyboard = new Scanner(System.in);
+        //initialization faze
+        Keyboard = new Scanner(System.in);
         System.out.println("type a number 1-100 and give me a question");
-    int firstNumber = Keyboard.nextInt();
-    if(10>=firstNumber&&firstNumber >0) {
-        System.out.println("the outlook looks very good");
-    }
-            if (20>=firstNumber&&firstNumber >11){
+        int firstNumber = Keyboard.nextInt();
+        if (10 >= firstNumber && firstNumber > 0) {
+            System.out.println("the outlook looks very good");
+        }
+        else if (20 >= firstNumber && firstNumber > 11) {
             System.out.println("The outlook doesn't look very good");
         }
-     if(30>=firstNumber&&firstNumber >21) {
-        System.out.println("yes");
-     }
-     if(40>=firstNumber&&firstNumber >31){
-        System.out.println("That will happen in the near future");
-     }
-    if(50>= firstNumber&&firstNumber >41){
-        System.out.println("That is very unlikely");
-    }
-    if(60>=firstNumber&&firstNumber >51){
-        System.out.println("no");
-    }
-    if(70>=firstNumber&&firstNumber >61){
-        System.out.println("probably");
-    }
-    if(80>=firstNumber&&firstNumber >71){
-        System.out.println("probably not");
-    }
-    if(90>=firstNumber&&firstNumber >81){
-        System.out.println("")
-    }
+        else if (30 >= firstNumber && firstNumber > 21) {
+            System.out.println("yes");
+        }
+        else if (40 >= firstNumber && firstNumber > 31) {
+            System.out.println("That will happen in the near future");
+        }
+        else if (50 >= firstNumber && firstNumber > 41) {
+            System.out.println("That is very unlikely");
+        }
+        else if (60 >= firstNumber && firstNumber > 51) {
+            System.out.println("no");
+        }
+        else if (70 >= firstNumber && firstNumber > 61) {
+            System.out.println("probably");
+        }
+        else if (80 >= firstNumber && firstNumber > 71) {
+            System.out.println("probably not");
+        }
+        else if (90 >= firstNumber && firstNumber > 81) {
+            System.out.println("not likely");
+        }
+        else if (100 >= firstNumber && firstNumber > 91) {
+            System.out.println("it is certain");
+        }
+        else System.out.println("not in the number range");
 
     }
 }
+
